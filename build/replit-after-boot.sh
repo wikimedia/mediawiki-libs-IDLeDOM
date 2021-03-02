80edbfbcfe33e4106b0d1c8211615228e1ca0b17
C. Scott Ananian <cscott@cscott.net>
2021-03-02 00:48:10 -0500

    Some additional improvements to the replit configuration
    Change-Id: I81b73e88256a44b76175a6cd7de19f1007ecfb63

--- build/replit-after-boot.sh
+++ build/replit-after-boot.sh
@@ -10,15 +10,35 @@ set -eu -o pipefail
 ROOTDIR=$(realpath $(dirname "$0")/..)
 echo Installing dependencies from $ROOTDIR
 cd $ROOTDIR
+chmod a+rx $ROOTDIR/build/helper
 
-install-pkg php-mbstring
-install-pkg php-curl
-export PHPRC=$ROOTDIR/build/replit.ini
-export PATH=$PATH:/home/runner/bin:$ROOTDIR/vendor/bin
+# Show configuration progress with a temporary directory we set up.
+mkdir .INSTALLING_1_of_4
+
+cd /home/runner
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 mkdir -p /home/runner/bin
 php composer-setup.php --install-dir=/home/runner/bin --filename=composer
 php -r "unlink('composer-setup.php');"
+cd $ROOTDIR
+
+mkdir .INSTALLING_2_of_4
+
+install-pkg php-mbstring
+
+mkdir .INSTALLING_3_of_4
+
+install-pkg php-curl
+
+mkdir .INSTALLING_4_of_4
+
+export PHPRC=$ROOTDIR/build/replit.ini
+export PATH=$PATH:/home/runner/bin:$ROOTDIR/vendor/bin
 
 /home/runner/bin/composer update
+
+rmdir .INSTALLING_1_of_4
+rmdir .INSTALLING_2_of_4
+rmdir .INSTALLING_3_of_4
+rmdir .INSTALLING_4_of_4
