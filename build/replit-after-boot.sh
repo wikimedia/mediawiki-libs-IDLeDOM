aeb4911954cb0f1960e0f83bd11e538e3ef2c808
C. Scott Ananian <cscott@cscott.net>
2021-03-02 00:34:38 -0500

    Fixes to replit configuration
    Change-Id: I1695c5fcf2621446414e99b566f811d8d68583e8

--- build/replit-after-boot.sh
+++ build/replit-after-boot.sh
@@ -7,12 +7,18 @@ set -eu -o pipefail
 
 # After boot, use 'build/helper' to reset the PHPRC and PATH appropriately
 
-pkg-install php-mbstring
-pkg-install php-curl
-export PHPRC=/home/runner/idle-dom/build/replit.ini
-export PATH=$PATH:/home/runner/bin:/home/runner/idle-dom/vendor/bin
+ROOTDIR=$(realpath $(dirname "$0")/..)
+echo Installing dependencies from $ROOTDIR
+cd $ROOTDIR
+
+install-pkg php-mbstring
+install-pkg php-curl
+export PHPRC=$ROOTDIR/build/replit.ini
+export PATH=$PATH:/home/runner/bin:$ROOTDIR/vendor/bin
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-mkdir -p ~/bin
+mkdir -p /home/runner/bin
 php composer-setup.php --install-dir=/home/runner/bin --filename=composer
 php -r "unlink('composer-setup.php');"
+
+/home/runner/bin/composer update
